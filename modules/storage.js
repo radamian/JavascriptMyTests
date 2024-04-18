f6dcf6e755ebc50b2da5f1d6c2e14af4b72c3be2
radamian <raluca.damian@endava.com>
2024-04-18 13:54:45 +0300

    Add files via upload

---
+++ modules/storage.js
@@ -0,0 +1,29 @@
+// Function to set cookie
+function setCookie(cookieName, cookieValue, days) {
+    let expires = "";
+    if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + (days * 10)); 
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = cookieName + "=" + (cookieValue || "") + expires + "; path=/";
+}
+
+// Function to get cookie value by name
+function getCookie(cookieName) {
+    let name = cookieName + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let cookieArray = decodedCookie.split(';');
+    for (let i = 0; i < cookieArray.length; i++) {
+        let cookie = cookieArray[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(name) === 0) {
+            return cookie.substring(name.length, cookie.length);
+        }
+    }
+    return "";
+}
+
+export{setCookie,getCookie};
